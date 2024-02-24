0bfb7373ee3a109eb6840801fd96a20f73c40bdc
slaurianodev <sergiolauriano@gmail.com>
2024-02-24 18:15:55 +0000

    Producer-consumer level 3 and some exercises

--- src/main/scala/lectures/part3concurrency/ThreadCommunication.scala
+++ src/main/scala/lectures/part3concurrency/ThreadCommunication.scala
@@ -138,6 +138,149 @@ object ThreadCommunication extends App {
     producer.start()
   }
 
-  prodConsLargeBuffer()
+//  prodConsLargeBuffer()
 
+  /*
+    Prod-cons, level 3
+
+      producer1 -> [? ? ? ?] -> consumer1
+      producer2 ----^     ^---- consumer2
+  */
+
+  class Consumer(id: Int, buffer: mutable.Queue[Int]) extends Thread {
+    override def run(): Unit = {
+      val random = new Random()
+
+      while (true) {
+        buffer.synchronized {
+          /*
+            producer produces value, two Cons are waiting
+            notifies ONE consumer, notifies on buffer
+            notifies the other consumer
+          */
+          while (buffer.isEmpty) {
+            println(s"[consumer $id] buffer empty, waiting...")
+            buffer.wait()
+          }
+
+          // there must be at least ONE value in the buffer
+          val x = buffer.dequeue()
+          println(s"[consumer $id] consumed " + x)
+
+          // hey producer, there's an empty space
+          buffer.notify()
+        }
+
+        Thread.sleep(random.nextInt(500))
+      }
+    }
+  }
+
+  class Producer(id: Int, buffer: mutable.Queue[Int], capacity: Int) extends Thread {
+    override def run(): Unit = {
+      val random = new Random()
+      var i = 0
+
+      while (true) {
+        buffer.synchronized {
+          while (buffer.size == capacity) {
+            println(s"[producer $id] buffer is full, waiting...")
+            buffer.wait()
+          }
+
+          // there must be at least ONE EMPTY SPACE in the buffer
+          println(s"[producer $id] producing " + i)
+          buffer.enqueue(i)
+
+          // hey consumer, new food for you
+          buffer.notify()
+
+          i += 1
+        }
+
+        Thread.sleep(random.nextInt(250))
+      }
+    }
+  }
+
+  def multiProdCons(nConsumers: Int, nProducers: Int): Unit = {
+    val buffer: mutable.Queue[Int] = new mutable.Queue[Int]
+    val capacity = 23
+
+    (1 to nConsumers).foreach(i => new Consumer(i, buffer).start())
+    (1 to nProducers).foreach(i => new Producer(i, buffer, capacity).start())
+  }
+
+//  multiProdCons(3, 6)
+
+  /*
+    Exercises.
+    1) Think of an example where notifyALL acts in a different way than notify
+    2) create a deadlock
+    3) create a livelock
+  */
+
+  // Exercise 1 - notifyAll
+  def testNotifyAll(): Unit = {
+    val bell = new Object
+    (1 to 10).foreach(i => new Thread(() => {
+      bell.synchronized {
+        println(s"[thread $i] waiting...")
+        bell.wait()
+        println(s"[thread $i] hooray!")
+      }
+    }).start())
+
+
+    new Thread(() => {
+      Thread.sleep(2000)
+      println("[announcer] Rock n' roll!")
+      bell.synchronized {
+        bell.notifyAll()
+      }
+    }).start()
+  }
+
+//  testNotifyAll()
+
+  // Exercise 2 - deadlock
+  case class Friend(name: String) {
+    def bow(other: Friend): Unit = {
+      this.synchronized {
+        println(s"$this: I am a bowing to my friend $other")
+        other.rise(this)
+        println(s"$this: my friend $other has risen")
+      }
+    }
+
+    def rise(other: Friend): Unit = {
+      this.synchronized {
+        println(s"$this: I am rising to my friend $other")
+      }
+    }
+
+    var side = "right"
+    def switchSide(): Unit = {
+      if (side == "right") side = "left"
+      else side = "right"
+    }
+
+    def pass(other: Friend): Unit = {
+      while (this.side == other.side) {
+        println(s"$this: Oh, but please, $other, feel free to pass...")
+        switchSide()
+        Thread.sleep(1000)
+      }
+    }
+  }
+
+  val sam = Friend("Sam")
+  val pierre = Friend("Pierre")
+
+//  new Thread(() => sam.bow(pierre)).start() // sam's lock     |   then pierre's lock
+//  new Thread(() => pierre.bow(sam)).start() // pierre's lock  |   then sam's lock
+
+  // Exercise 3 - livelock
+  new Thread(() => sam.pass(pierre)).start()
+  new Thread(() => pierre.pass(sam)).start()
 }
